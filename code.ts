4664fa33c176cc7bae30117cd723c401466c743f
Yoshihiro Tanaka <contact@cordea.jp>
2021-11-27 16:40:55 +0900

    parse text

--- code.ts
+++ code.ts
@@ -1,15 +1,21 @@
-const text = figma.currentPage.selectedTextRange;
-if (!text) {
-  figma.closePlugin();
+import { loadDefaultJapaneseParser } from 'budoux';
+
+function updateText(node: TextNode) {
+  const parser = loadDefaultJapaneseParser();
+  const rawText = node.characters;
+  node.characters = parser.parse(rawText).join('\n');
 }
 
-async function updateText() {
+async function run() {
+  const text = figma.currentPage.selectedTextRange;
+  if (!text) {
+    figma.closePlugin();
+    return;
+  }
   const font = text.node.fontName;
-  await figma.loadFontAsync(font as FontName);
-
-  const rawText = text.node.characters;
-  text.node.characters = rawText;
+  await figma.loadFontAsync(font as FontName)
+  updateText(text.node);
   figma.closePlugin();
 }
 
-updateText();
+run();
